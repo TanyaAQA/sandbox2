2637f2d44f9f6f989ae38feada09fe1cd9379f87
Herandnu <tanya.rfdm@hotmail.com>
2023-06-11 23:47:20 +0300

    добавлено несколько функций

--- src/main/java/ru/stqa/pft/sandbox/MyFirstSoft.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstSoft.java
@@ -2,6 +2,27 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstSoft {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        Hello(123);
+
+        int len = 5;
+      System.out.println("Площадь квадрата со стороной " + len + " = " + ploshad(len));
+
+
+      double storona1 = 4;
+      double storona2 = 6;
+      System.out.println("Площадь прямоуга со сторонами " + storona1 + "и" + storona2 + " = " + area(storona1, storona2));
+
+    }
+
+    public static void Hello (int somebody) {
+           System.out.println("Hello " + somebody + "!");
+    }
+
+    public static int ploshad (int a) {
+      return a * a;
+    }
+
+    public static double area (double b, double c) {
+      return b * c;
     }
 }
